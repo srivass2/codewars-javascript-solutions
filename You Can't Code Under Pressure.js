7067ec9f8e56a800a9b89411be2ccb557ca01308
srivass2 <shashank.srivastava2@mail.dcu.ie>
2025-08-07 20:35:42 +0100

    solution

---
+++ You Can't Code Under Pressure.js
@@ -0,0 +1,13 @@
+/*
+Description:
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+
+const doubleFun = (num) => {
+    
+        return num + num ;
+    
+}
+
+console.log(doubleFun(2));
